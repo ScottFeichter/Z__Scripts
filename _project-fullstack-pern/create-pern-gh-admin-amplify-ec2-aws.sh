15302bdc338bb2a9aef8fd143ecf8273eca0ba3b
Scott Feichter <scottfeichter@gmail.com>
2025-02-20 06:11:51 -0800

    misc (msg via sh alias)

--- _project-fullstack-pern/create-pern-gh-admin-amplify-ec2-aws.sh
+++ _project-fullstack-pern/create-pern-gh-admin-amplify-ec2-aws.sh
@@ -1301,7 +1301,7 @@ cat > .sequelizerc << EOL
 const path = require('path');
 
 module.exports = {
-  'config': path.resolve('config', 'database.ts'),
+  'config': path.resolve('config', 'database.js'),
   'models-path': path.resolve('db', 'models'),
   'seeders-path': path.resolve('db', 'seeders'),
   'migrations-path': path.resolve('db', 'migrations')
